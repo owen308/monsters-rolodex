6c5356196be9c2d7194f818a1145331a4c0b0c56
Owen <oholmes77@gmail.com>
2020-08-08 22:25:49 +1000

    Updates

---
+++ precache-manifest.2cd060ea690361a5705387655e7d3cef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7c92fb4ff8649d1799a4ce6073ce6e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "049947648433c3eba59c",
+    "url": "/monsters-rolodex/static/css/main.3e103e21.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049947648433c3eba59c",
+    "url": "/monsters-rolodex/static/js/main.228ba3c6.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
